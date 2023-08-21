bbe6548b08a2654f626aad04c2a0151cec160e6f
Gabibosss <libertycity2468@gmail.com>
2023-08-21 21:00:40 +0000

    new changes

--- hospital/models.py
+++ hospital/models.py
@@ -30,7 +30,7 @@ class Doctor(models.Model):
 
 class Patient(models.Model):
     user=models.OneToOneField(User,on_delete=models.CASCADE)
-    profile_pic= models.ImageField(upload_to='profile_pic/PatientProfilePic/',default="default.png")
+    profile_pic= models.ImageField(upload_to='profile_pic/PatientProfilePic/',null=True, blank=True)
     address = models.CharField(max_length=40)
     mobile = models.CharField(max_length=20,null=False)
     symptoms = models.CharField(max_length=100,null=False)
